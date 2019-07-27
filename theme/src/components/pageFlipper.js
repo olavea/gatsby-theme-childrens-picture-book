87d4d3bfb4d3cb5772c2099f2c2d9f18c83ba4c5
Ola Vea <ola@olavea.com>
2019-07-27 09:51:52 +0200

    Made PageFlipper a functional component, bulletpoint 1 in issue #32

--- theme/src/components/pageFlipper.js
+++ theme/src/components/pageFlipper.js
@@ -2,60 +2,36 @@ import React from "react"
 
 import { Link, navigate } from "gatsby"
 
-class PageFlipper extends React.Component {
-  handleKeyStroke = event => {
-    const { nextPage, previousPage } = this.props
+const PageFlipper = ({ nextPage, previousPage }) => {
+  return (
+    <ul
+      style={{
+        background: `pink`,
+        display: `flex`,
+        flexWrap: `wrap`,
+        justifyContent: `space-between`,
+        listStyle: `none`,
 
-    if (event.keyCode === 37 && previousPage) {
-      navigate(previousPage.path)
-    }
-
-    if (event.keyCode === 39 && nextPage) {
-      navigate(nextPage.path)
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener("keydown", this.handleKeyStroke)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.handleKeyStroke)
-  }
-
-  render() {
-    const { nextPage, previousPage } = this.props
-
-    return (
-      <ul
-        style={{
-          background: `pink`,
-          display: `flex`,
-          flexWrap: `wrap`,
-          justifyContent: `space-between`,
-          listStyle: `none`,
-
-          margin: 0,
-          marginBottom: 0,
-        }}
-      >
-        <li>
-          {previousPage && (
-            <Link to={`/${previousPage.path}`} rel="prev">
-              ← {previousPage.title}
-            </Link>
-          )}
-        </li>
-        <li>
-          {nextPage && (
-            <Link to={`/${nextPage.path}`} rel="next">
-              {nextPage.title} →
-            </Link>
-          )}
-        </li>
-      </ul>
-    )
-  }
+        margin: 0,
+        marginBottom: 0,
+      }}
+    >
+      <li>
+        {previousPage && (
+          <Link to={`/${previousPage.path}`} rel="prev">
+            ← {previousPage.title}
+          </Link>
+        )}
+      </li>
+      <li>
+        {nextPage && (
+          <Link to={`/${nextPage.path}`} rel="next">
+            {nextPage.title} →
+          </Link>
+        )}
+      </li>
+    </ul>
+  )
 }
 
 export default PageFlipper
